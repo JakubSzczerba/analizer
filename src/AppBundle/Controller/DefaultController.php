2259c573d48e9632632b06d39f1762147ec1bf02
JakubSzczerba <jakszczerb@gmail.com>
2022-12-06 21:35:52 +0100

    Add parsing csv file to array with product_id and price

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,9 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use AppBundle\Form\SaleType;
+use AppBundle\Services\DataProvider;
 
 class DefaultController extends Controller
 {
+    private DataProvider $dataPovider;
+
+    public function __construct(DataProvider $dataPovider)
+    {
+        $this->dataPovider = $dataPovider;
+    }
+
     /**
      * @Route("/", name="homepage")
      */
@@ -21,15 +29,43 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/form", name="homepage")
+     * @Route("/form", name="form")
      */
     public function uploadFile(Request $request)
     {
         $form = $this->createForm(SaleType::class);
         $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $date = $form->get('dateAdd')->getData();
+            $status = $form->get('status')->getData();
+            $file = $form->get('file')->getData();
+
+            $result = $this->dataPovider->serializeData($file);
+            $price = [];
+            foreach ($result as $key => $row)
+            {
+                $price[$row['product_id']] = $row['price'];
+            }
+            //array_multisort($price, SORT_DESC, $result);
+            //$res = array_slice($price, 0, 10);
+
+            //price -> array with id and price -> need add sorting via dates etc
+
+            //return $this->redirectToRoute('analize');
+        }
+
         return $this->render('form/uploadFile.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/analize", name="analize")
+     */
+    public function showAnalize(Request $request)
+    {
+        return $this->render('default/index.html.twig', []);
     }
 }
---
+++ src/AppBundle/Services/DataProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Services;
+
+class DataProvider
+{
+    public function serializeData($file): array
+    {
+        $open = fopen($file->getPathname(), "r");
+        $data = fgetcsv($open);
+        $rows = [];
+        $header = [];
+        $index = 0;
+        while (($line = fgetcsv($open)) !== FALSE) {
+            if ($index == 0) {
+                $header = $line;
+                $index = 1;
+            } else {
+                $row = [];
+                for ($i = 0; $i < count($header); $i++) {
+                    $row[$data[$i]] = $line[$i];
+                }
+                array_push($rows, $row);
+            }
+        }
+
+        return $rows;
+    }
+
+}
